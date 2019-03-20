d80e8dcd670f2cfae53ae73e2166656ee3ffe21e
Patrick Bryan Corambao <patpatcorambao2@gmail.com>
2019-03-21 00:10:27 +0800

    handsome si PATRICK

---
+++ application/views/home.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title> Lab activity Git and Git HUB </title>
+<link href="assets/css/mystyle.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<h1> PINEDA GIT & GITHUB ACTIVITY </h1>
+	<script src="<?php echo "assets/js/myjs.js"; ?>" ></script>
+</body>
+</html>
